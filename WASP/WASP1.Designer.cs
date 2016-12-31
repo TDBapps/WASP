bc38cc8c81fbe7449d752ba4b79f788c7dded70b
TheDarkBomber <thedarkbomber80@gmail.com>
2016-12-31 23:32:12 +0000

    Add WASP icon

--- WASP/WASP1.Designer.cs
+++ WASP/WASP1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(WASP1));
             this.label1 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
@@ -59,6 +60,7 @@
             this.ClientSize = new System.Drawing.Size(324, 101);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "WASP1";
             this.Text = "WASP";
             this.ResumeLayout(false);
